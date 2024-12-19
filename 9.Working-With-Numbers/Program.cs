4babc3bae086a47ca62600240aa73ea3d6cce262
Jaber Khan <md.jaberkhan66@gmail.com>
2024-12-20 01:27:20 +0600

    C#-Working-With-Numbers

---
+++ 9.Working-With-Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿double num1 = 10.32D;
+double num2 = 20.4345D;
+double c =  num1 / num2;
+
+Console.WriteLine("C value is: {0}", c); // positional value
+Console.WriteLine($"C value is: {c}"); // interpolatrion value
+Console.WriteLine(string.Format("{0:0.00}", c)); // formating precision 0.00
+Console.WriteLine(string.Format("{0:0.0}", c)); // formating precision 0.0
+
+
+
